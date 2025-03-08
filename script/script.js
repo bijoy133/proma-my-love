99bd0c0adb0a0263e0f0feb89fdec70e9d0b536f
bijoy133 <bijoydas5680@!gmail.com>
2025-03-08 17:47:34 +0600

    added a commit

--- script/script.js
+++ script/script.js
@@ -4,7 +4,7 @@
 const indexBtn =document.getElementById('image-btn');
 if(indexBtn){
   indexBtn.addEventListener('click',function(){
-    window.location.href="image.html"
+    window.location.href="quesion.html"
   })
 }
 
@@ -12,7 +12,7 @@ if(indexBtn){
 const buttons =document.getElementById('homeBtn');
 if(buttons){
   buttons.addEventListener('click',function(){
-    window.location.href="http://127.0.0.1:5500/index..html"
+    window.location.href="index.html"
     
   })
 }
